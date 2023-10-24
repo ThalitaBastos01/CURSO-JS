09958c9e828a52d9a4dc04f2f11e5edc86f2fe20
Thalita <thalita.bastos94@gmail.com>
2023-10-24 16:59:11 -0300

    tentando realizar o desafio

---
+++ aula009/ex012.js
@@ -0,0 +1,9 @@
+var idade = 68
+if (idade < 16) {
+    console.log('não vota')
+} else if(idade < 18 || idade > 65) {
+    console.log('voto opcional')
+} else {
+    console.log('voto obrigatorio')
+}
+
