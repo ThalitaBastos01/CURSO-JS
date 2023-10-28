5b0a0b33cfcb5a31417c5bbd3ef9a4e70f4d1b4a
Thalita <thalita.bastos94@gmail.com>
2023-10-28 20:57:10 -0300

    variaveis e if

--- Alura/introducao-javascript/js/principal.js
+++ Alura/introducao-javascript/js/principal.js
@@ -2,6 +2,31 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
-paciente.querySelector(".info-peso")
 
-console.log(paciente);
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true; 
+
+if(peso <= 0 || peso > 400) {
+    console.log("Peso invalido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida");
+    alturaEhValida = false; 
+    tdImc.textContent = "Altura inválido!";
+}
+
+if(alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
