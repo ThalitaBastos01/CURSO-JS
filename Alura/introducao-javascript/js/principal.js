3be3117f6ad5082feaba1df6eda0e61cf668c88e
Thalita <thalita.bastos94@gmail.com>
2023-10-28 12:19:01 -0300

    querySelector

---
+++ Alura/introducao-javascript/js/principal.js
@@ -0,0 +1,7 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+paciente.querySelector(".info-peso")
+
+console.log(paciente);
