edc983af335a9202c5ba7efdb6062f43424ebf7d
Thalita coelho <thalita.bastos94@gmail.com>
2023-11-23 13:37:48 -0300

    Update app.js

--- Alura/logica-js-projeto_inicial/app.js
+++ Alura/logica-js-projeto_inicial/app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 10;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
+// se o chute for igual ao número secreto
 if (numeroSecreto == chute) {
-    console.log('Isso ai! Você descobriu o numero secreto')
+    alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto}`)
+} else {
+    alert('Você errou :(')
 }
